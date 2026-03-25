7a61f4c4bd16c13b2ac4d4c4b342cef1e6bedb86
JoshFransix <joshiandersonk69@gmail.com>
2026-03-25 16:33:34 +0000

    feat: implement ScrollToTop component and enhance Summary component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -6,3 +6,4 @@ export { DarkModeToggle } from './DarkModeToggle';
 export { ComparisonCard } from './ComparisonCard';
 export { ScenarioExplorer } from './ScenarioExplorer';
 export { ProbabilityAnalyzer } from './ProbabilityAnalyzer';
+export { ScrollToTop } from './ScrollToTop';
